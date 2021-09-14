8251aeb30e3f84a7e004a3b2b4c5ea60ebbaee3e
Juniper Song <songminseonk@gmail.com>
2021-09-14 12:39:38 -0400

    UI and Models project, basic menu

---
+++ 01CSharp/RestaurantReviews-Console/Models/Restaurant.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Models
+{
+    public class Restaurant
+    {
+        //default empty constructor
+        public Restaurant() {}
+
+        //constructor overloading
+        public Restaurant(string name)
+        {
+            this.Name = name;
+        }
+
+        //Constructor chaining
+        public Restaurant(string name, string city, string state) : this(name)
+        {
+            this.City = city;
+            this.State = state;
+        }
+
+        //this is type member
+        //this is an example of property
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        // //this is field
+        // private string _name;
+
+        // //this is a wrapper for the field above
+        // public string GetName()
+        // {
+        //     return _name;
+        // }
+
+        // public void SetName(string value)
+        // {
+        //     _name = value;
+        // }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, City: {this.City}, State: {this.State}";
+        }
+    }
+}
---
+++ 01CSharp/RestaurantReviews-Console/UI/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Models;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            string input = "";
+            do
+            {
+                Console.WriteLine("Welcome to Restaurant Reviews!");
+                Console.WriteLine("[0] Manage Restaurants");
+                Console.WriteLine("[1] Leave Reviews");
+                Console.WriteLine("[x] Exit");
+
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "0":
+                        Console.WriteLine("You wanted to manage restos");
+                        break;
+
+                    case "1":
+                        Console.WriteLine("you wanted to leave reviews");
+                        break;
+
+                    case "x":
+                        Console.WriteLine("Goodbye!");
+                        exit = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("I don't know what you're talking about");
+                        break;
+                }
+            } while (!exit);
+        }
+    }
+}
