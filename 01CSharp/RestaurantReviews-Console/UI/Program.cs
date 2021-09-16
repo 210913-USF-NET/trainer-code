4766857a83c9b1106bb7a3b299cc81e9dd5fa301
Juniper Song <songminseonk@gmail.com>
2021-09-16 16:01:10 -0400

    Serilog

--- 01CSharp/RestaurantReviews-Console/UI/Program.cs
+++ 01CSharp/RestaurantReviews-Console/UI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Serilog;
 
 namespace UI
 {
@@ -6,7 +7,27 @@ namespace UI
     {
         static void Main(string[] args)
         {
+            // using(var logger = new LoggerConfiguration()
+            // .MinimumLevel.Debug()
+            // .WriteTo.Console()
+            // .WriteTo.File("../logs/logs.txt", rollingInterval: RollingInterval.Day)
+            // .CreateLogger()){
+            //     //I have access to my logger in this block
+            //     //but once I exit,
+            //     //my logger is disposed
+            // }
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("../logs/logs.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+            Log.Information("Application Starting...");
+            
             new MainMenu().Start();
+
+            Log.Information("Application closing...");
+            Log.CloseAndFlush();
         }
     }
 }
