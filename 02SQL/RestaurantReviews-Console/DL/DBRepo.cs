aaad0a5c41e2d605bd779a482658a2dd35e1a4b1
Juniper Song <songminseonk@gmail.com>
2021-09-21 17:25:13 -0400

    DB Repo

---
+++ 02SQL/RestaurantReviews-Console/DL/DBRepo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class DBRepo : IRepo
+    {
+        //this needs dbcontext
+        //and these methods will get data from db and persist to db
+        
+    }
+}
